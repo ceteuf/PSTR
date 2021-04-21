42fb53b5f04a1ec55fecf4bd8ef5c5a63b83f175
ceteuf <charles.teuf@gmail.com>
2021-04-21 15:07:26 +0200

    up

--- pstr.py
+++ pstr.py
@@ -62,6 +62,63 @@ class PSTR(object):
         self.vYb = vYb
 
 
+    def lmtest(self, vU, mW, mM, s2, mX2, invXX):
+        df1 = mW.shape[1]
+        df2 = (self.t*self.i) - df1 - self.i - self.mX.shape[1]
+        #print(self.t, self.i, df1, self.mX.shape[1])
+        mW2 = mM @ mW
+        mXW2 = np.transpose(mX2) @ mW2
+        #self.mXW2 = mXW2
+        # S1 = ( crossprod(mW2) - t(mXW2) %*% invXX %*% mXW2 ) * s2
+        #print(s2)
+        S1 = (np.transpose(mW2) @ mW2 - (np.transpose(mXW2) @ invXX @ mXW2 )) * s2  # 
+        self.S1 = S1
+        ttmp = np.linalg.svd(S1)
+        self.ttmp = ttmp
+        # try:
+        #     invS1 = np.linalg.inv(np.linalg.cholesky(S1))
+        # except:
+        #     ttmp = np.linalg.svd(S1)
+        #     invS1 = ttmp.u 
+
+
+    def lintest(self):
+        mD = np.kron(np.eye(self.i),  np.repeat(1, self.t)[:, np.newaxis])
+        mM = np.eye(self.i*self.t) - (mD @ np.transpose(mD)) / self.t
+        mX2 = mM @ self.mX
+        self.mX2 = mX2
+        
+        tmp2 = cholesky(np.transpose(mX2) @ mX2, lower=False)
+        self.tmp2 = tmp2
+        #invXX = np.linalg.inv(cholesky( np.transpose(mX2) @ mX2, lower=False ) )
+        invXX = np.linalg.inv( np.transpose(mX2) @ mX2)
+        self.invXX = invXX
+        
+        #tmp = chol2inv(chol(crossprod(mXb))) %*% crossprod(mXb, vYb)
+        #tmp = np.linalg.inv(np.linalg.cholesky(  np.transpose(self.mXb) @ self.mXb  ))  @  (np.transpose(self.mXb) @ self.vYb)
+        # PLUS DE CORRESP AVEC R a partir d'ici
+        #tmp = np.linalg.inv(np.transpose(self.mXb) @ self.mXb )  #  @  (np.transpose(self.mXb) @ self.vYb)
+        #tmp = inv( np.transpose(self.mXb) @ self.mXb )
+        A = np.transpose(self.mXb) @ self.mXb
+        print(A.shape)
+        tmp = np.linalg.solve(A, np.eye(17) )
+        self.tmp3 = tmp
+"""         vU = np.reshape(self.vY - (self.mX @ tmp), (self.t, self.i))
+        vU = np.transpose(np.transpose(vU) - np.mean(np.transpose(vU), axis=1, keepdims=True)).flatten()[:, np.newaxis]
+        self.vU = vU
+        s2 = sum((vU - np.mean(vU, keepdims=True)) ** 2)   /(self.i*self.t)
+        print(s2)
+        coln = np.transpose([np.arange(1, self.i+1)] * self.t)
+        coln = np.reshape(coln, coln.size)
+        for qter in range(0, self.mQ.shape[1]):
+            vQ = self.mQ[:, qter][:, np.newaxis]
+            mW = self.mK*vQ
+            self.lmtest(vU, mW, mM, s2, mX2, invXX)
+            break
+
+        self.s2 = s2 """
+        #invXX = np.linalg.inv(np.linalg.cholesky(mX2))
+        #self.invXX = invXX
 
 # %%
 
@@ -77,3 +134,15 @@ new_pstr = PSTR(data=df,
                 timeVar="year",
                 indiVar="cusip")
 # %%
+new_pstr.mXb.shape
+# %%
+new_pstr.lintest()
+
+# %%
+new_pstr.invXX[0:5, 0:5]
+# %%
+new_pstr.tmp2[0:5, 0:5]
+# %%
+np.eye(17) 
+# %%
+new_pstr.tmp3[0:5, 0:5]
