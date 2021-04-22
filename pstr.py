435596b40080a982b45fd9f846e53ca2d794565c
ceteuf <charles.teuf@gmail.com>
2021-04-22 18:04:48 +0200

    up

--- pstr.py
+++ pstr.py
@@ -2,7 +2,10 @@
 from utils import MaClasse
 import pandas as pd 
 import numpy as np
-from scipy.linalg import cholesky, inv
+from scipy.linalg import cholesky, inv, cho_solve, cho_factor
+import mpmath as mp
+np.set_printoptions(precision=8)
+mp.mp.dps = 50
 # %%
 def checkIfElsInColDf(cols, lst):
     for el in lst:
@@ -42,6 +45,7 @@ class PSTR(object):
         self.t = len(self.t_dim)
         self.i = len(self.i_dim)
 
+
         self.vY = data[dep].values # independante
         self.mX = data[indeps].values # dépendantes
         self.mK = data[indeps_k].values
@@ -60,6 +64,7 @@ class PSTR(object):
             mXb = np.vstack([mXb, val])
         self.mXb = mXb
         self.vYb = vYb
+        self.vY = self.vY[:, np.newaxis]
 
 
     def lmtest(self, vU, mW, mM, s2, mX2, invXX):
@@ -86,46 +91,45 @@ class PSTR(object):
         mD = np.kron(np.eye(self.i),  np.repeat(1, self.t)[:, np.newaxis])
         mM = np.eye(self.i*self.t) - (mD @ np.transpose(mD)) / self.t
         mX2 = mM @ self.mX
-        self.mX2 = mX2
-        
-        tmp2 = cholesky(np.transpose(mX2) @ mX2, lower=False)
-        self.tmp2 = tmp2
-        #invXX = np.linalg.inv(cholesky( np.transpose(mX2) @ mX2, lower=False ) )
         invXX = np.linalg.inv( np.transpose(mX2) @ mX2)
         self.invXX = invXX
-        
-        #tmp = chol2inv(chol(crossprod(mXb))) %*% crossprod(mXb, vYb)
-        #tmp = np.linalg.inv(np.linalg.cholesky(  np.transpose(self.mXb) @ self.mXb  ))  @  (np.transpose(self.mXb) @ self.vYb)
-        # PLUS DE CORRESP AVEC R a partir d'ici
-        #tmp = np.linalg.inv(np.transpose(self.mXb) @ self.mXb )  #  @  (np.transpose(self.mXb) @ self.vYb)
-        #tmp = inv( np.transpose(self.mXb) @ self.mXb )
-        A = np.transpose(self.mXb) @ self.mXb
-        print(A.shape)
-        tmp = np.linalg.solve(A, np.eye(17) )
-        self.tmp3 = tmp
-"""         vU = np.reshape(self.vY - (self.mX @ tmp), (self.t, self.i))
-        vU = np.transpose(np.transpose(vU) - np.mean(np.transpose(vU), axis=1, keepdims=True)).flatten()[:, np.newaxis]
-        self.vU = vU
-        s2 = sum((vU - np.mean(vU, keepdims=True)) ** 2)   /(self.i*self.t)
-        print(s2)
-        coln = np.transpose([np.arange(1, self.i+1)] * self.t)
-        coln = np.reshape(coln, coln.size)
-        for qter in range(0, self.mQ.shape[1]):
-            vQ = self.mQ[:, qter][:, np.newaxis]
-            mW = self.mK*vQ
-            self.lmtest(vU, mW, mM, s2, mX2, invXX)
-            break
+        rr = np.transpose(self.mXb) @ self.mXb
+        vv = np.transpose(self.mXb) @ self.vYb
+        tmp = np.linalg.inv(rr) @ vv
+        #check = np.transpose(self.mXb) @ self.vYb
+        #check2 = np.linalg.inv(rr)
+        # print(np.linalg.cond(np.transpose(self.mXb) @ self.mXb, p=2))
+        tmp = tmp[:, np.newaxis]
+        self.rr = rr
+        self.tmp = tmp
+        #self.check = check
+        #self.check2 = check2
+        # vU = np.reshape(self.vY - (self.mX @ tmp), (self.t, self.i))
+        # vU_tmp = self.vY - (self.mX @ tmp)
+        # vU = np.transpose(np.transpose(vU) - np.mean(np.transpose(vU), axis=1, keepdims=True)).flatten()[:, np.newaxis]
+        # self.vU_tmp = vU_tmp
+        # self.vU = vU
+        # s2 = sum((vU - np.mean(vU, keepdims=True)) ** 2)   /(self.i*self.t)
+        # print(s2)
+        # coln = np.transpose([np.arange(1, self.i+1)] * self.t)
+        # coln = np.reshape(coln, (coln.size, 1))
+        # self.coln = coln
+        # for qter in range(0, self.mQ.shape[1]):
+        #    vQ = self.mQ[:, qter][:, np.newaxis]
+        #    mW = self.mK*vQ
+        #    self.lmtest(vU, mW, mM, s2, mX2, invXX)
+        #    break
 
-        self.s2 = s2 """
+        #self.s2 = s2
         #invXX = np.linalg.inv(np.linalg.cholesky(mX2))
         #self.invXX = invXX
 
 # %%
 
 # %%
-df = pd.read_csv("data/hansen99.csv")
-# %%
+df = pd.read_csv("data/hansen99.csv") # .astype(np.float32)
 
+# %%
 new_pstr = PSTR(data=df,
                 dep='inva', 
                 indeps=list(df.columns[3:21]), 
@@ -134,14 +138,33 @@ new_pstr = PSTR(data=df,
                 timeVar="year",
                 indiVar="cusip")
 # %%
-new_pstr.tmp
-# %22new_pstr.lintest()
+new_pstr.invXX[0:3, 0:3]
+# %%
+new_pstr.lintest()
+# %%
+new_pstr.coln.shape
+# %%
+df.columns[3:21]
+# %%
+np.linalg.inv(new_pstr.rr)[0:3, 0:3]
+# %%
+np.linalg.solve(new_pstr.rr, np.eye(17))[0:3, 0:3]
+# %%
+c = np.linalg.inv(np.linalg.cholesky(new_pstr.rr))
+np.dot(c.T,c)[0:3, 0:3]
+# %%
+A = new_pstr.rr
+c, low = cho_factor(A)
+
+x = cho_solve((cho_factor(A), False), np.eye(17))[0:3, 0:3]
 
 # %%
-new_pstr.invXX[0:5, 0:5]
+from rpy2.robjects.packages import importr
+from rpy2.robjects import numpy2ri
+numpy2ri.activate()
+# import R's "base" package
+base = importr('float')
 # %%
-new_pstr.tmp2[0:5, 0:5]
+base.chol2inv(base.chol(A))[0:2,0:2]
 # %%
-np.eye(17) 
-# %%
-new_pstr.tmp3[0:5, 0:5]
+np.linalg.cond(new_pstr.rr, p=2)
